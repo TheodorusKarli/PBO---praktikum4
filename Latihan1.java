c6f74e3e8166203926a9c181052e31eff2350a90
Theodorus Karli <90240274+TheodorusKarli@users.noreply.github.com>
2021-10-28 11:09:05 +0800

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,19 @@
+class Siswa { int nrp;
+
+	public void setNrp(int i) { 
+	nrp=i;
+	}
+	
+}
+
+public class Latihan1 {
+	
+	public static void main(String args[]) { 
+
+	Siswa anak=new Siswa(); 
+	anak.setNrp(5); 
+	System.out.println(anak.nrp);
+
+	}
+	
+}
